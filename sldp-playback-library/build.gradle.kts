29ba8056231f2f979f20c3632b13e25b8845a67b
Sergey Ignatov <signatov@wmspanel.com>
2025-08-08 16:05:42 +1000

    Depend on bundleReleaseAar

--- sldp-playback-library/build.gradle.kts
+++ sldp-playback-library/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.android.build.gradle.BaseExtension
+import org.gradle.kotlin.dsl.the
+
 plugins {
     alias(libs.plugins.android.library)
     id("maven-publish")
@@ -32,21 +35,34 @@ dependencies {
     implementation(libs.androidx.annotations)
 }
 
-configure<PublishingExtension> {
-    publications.create<MavenPublication>("sldp-playback-library") {
-        groupId = "com.github.sergeiignatov"
-        artifactId = "sldp-playback-library"
-        version = "0.0.1-alpha01"
-        pom.packaging = "aar"
-        artifact("$buildDir/outputs/aar/sldp-playback-library-release.aar")
+tasks.register<Jar>("sourcesJar") {
+    //    from(android.sourceSets.getByName("main").java.sourceFiles)
+    from(project.the<BaseExtension>().sourceSets["main"].java.srcDirs)
+    archiveClassifier.set("sources")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.sergeiignatov"
+            artifactId = "sldp-playback-library"
+            version = "0.0.1-alpha01"
+
+            artifact(tasks["sourcesJar"])
+            afterEvaluate { artifact(tasks.getByName("bundleReleaseAar")) }
+
+            pom.withXml {
+                val dependenciesNode = asNode().appendNode("dependencies")
+                configurations.implementation.get().allDependencies.forEach {
+                    val dependencyNode = dependenciesNode.appendNode("dependency")
+                    dependencyNode.appendNode("groupId", it.group)
+                    dependencyNode.appendNode("artifactId", it.name)
+                    dependencyNode.appendNode("version", it.version)
+                }
+            }
+        }
     }
     repositories {
         mavenLocal()
-    }
-}
-
-afterEvaluate {
-    tasks.withType(PublishToMavenRepository::class.java) {
-        dependsOn(tasks.assemble)
     }
 }
