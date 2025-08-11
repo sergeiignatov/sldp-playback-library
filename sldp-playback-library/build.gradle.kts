4904d8c61bdf00e426c5bf9ed44f73d00cdc453b
Sergey Ignatov <signatov@wmspanel.com>
2025-08-11 12:32:08 +1000

    Publish to jitpack.io

--- sldp-playback-library/build.gradle.kts
+++ sldp-playback-library/build.gradle.kts
@@ -1,5 +1,9 @@
+import com.android.build.gradle.BaseExtension
+import org.gradle.kotlin.dsl.the
+
 plugins {
     alias(libs.plugins.android.library)
+    id("maven-publish")
 }
 
 android {
@@ -21,12 +25,39 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
-    buildFeatures {
-        buildConfig = true
-    }
 }
 
 dependencies {
     implementation(libs.androidx.annotations)
+}
+
+tasks.register<Jar>("sourcesJar") {
+    from(project.the<BaseExtension>().sourceSets["main"].java.srcDirs)
+    archiveClassifier.set("sources")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.softvelum"
+            artifactId = "sldp-playback-library"
+            version = "0.0.1"
+
+            artifact(tasks["sourcesJar"])
+            afterEvaluate { artifact(tasks.getByName("bundleReleaseAar")) }
+
+            pom.withXml {
+                val dependenciesNode = asNode().appendNode("dependencies")
+                configurations.implementation.get().allDependencies.forEach {
+                    val dependencyNode = dependenciesNode.appendNode("dependency")
+                    dependencyNode.appendNode("groupId", it.group)
+                    dependencyNode.appendNode("artifactId", it.name)
+                    dependencyNode.appendNode("version", it.version)
+                }
+            }
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
 }
